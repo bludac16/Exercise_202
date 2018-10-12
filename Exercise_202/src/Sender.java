73d93847fb930ee77cc1594dee804c457ce03523
David Blümel <bludac16@htl-kaindorf.ac.at>
2018-10-12 12:15:23 +0200

    Created: project & all classes

--- Exercise_202/src/Sender.java
+++ Exercise_202/src/Sender.java
@@ -9,5 +9,39 @@
  * @author David
  */
 public class Sender {
+    private String name;
+    private double frequenz;
+    private String band;
+
+    public Sender(String name, double frequenz, String band) {
+        this.name = name;
+        this.frequenz = frequenz;
+        this.band = band;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getFrequenz() {
+        return frequenz;
+    }
+
+    public void setFrequenz(double frequenz) {
+        this.frequenz = frequenz;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+    
     
 }
