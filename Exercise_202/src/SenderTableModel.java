5b4d16dcc3a4f6a33a3c991f98ba265a5d32344c
David Blümel <bludac16@htl-kaindorf.ac.at>
2018-10-12 12:44:10 +0200

    worked on class: TableModel

--- Exercise_202/src/SenderTableModel.java
+++ Exercise_202/src/SenderTableModel.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +12,45 @@
  *
  * @author David
  */
-public class SenderTableModel {
+public class SenderTableModel extends AbstractTableModel{
+    private static String[] colNames = {};
+    private ArrayList<Sender> senderliste = new ArrayList<>();
+
+    public void addSender(Sender s) {
+        senderliste.add(s);
+        fireTableDataChanged();
+    }
+
+    public void updateNumCols(boolean band) {
+        if(band == false)
+        {
+            colNames = new String[2];
+        } else{
+            colNames = new String[3];
+        }
+
+        fireTableStructureChanged();
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return colNames[i];
+    }
+
+    @Override
+    public int getRowCount() {
+        return senderliste.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Sender s = senderliste.get(row);
+        return s;
+    }
     
 }
